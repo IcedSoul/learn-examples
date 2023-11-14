53a4ad0aada6c95946a9ba10f73422e423b9faea
Xiaofeng Guo <xiaofeng.guo@unity.cn>
2023-11-14 10:57:15 +0800

    init: init c# wasm example.

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+// Create a "Main" method. This is required by the tooling.
+return;
+
+public partial class Sample
+{
+    // Make the method accessible from JS
+    [JSExport]
+    internal static int Add(int a, int b)
+    {
+        return a + b + 1;
+    }
+}
